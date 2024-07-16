14823486257b734d78357de720b1faa94e784251
Louise Keinström <louise.keinstrom@outlook.com>
2024-07-16 16:54:33 +0200

    Learnt about print and input as well as variables and concatenations within print

---
+++ basics.py
@@ -0,0 +1,41 @@
+# # EXECUTION ORDERS
+# print('line 1')
+# print('line 2')
+# print('line 3')
+
+# # DATATYPEs
+# print('string')
+# print(123) #integers /ints
+# print(1.23) #floating point value / float
+
+# # OPERATIONS
+# print(3+3)
+# print(3-3)
+# print(3*3)
+# print(3/3)
+
+# # cant add 10 + "hello", but 10 * "hello"
+# # print(10*"hello")
+
+# # VARIABLES
+# test = 123
+# print(test)
+# print(test + 10)
+# test = test + 50
+# print(test)
+
+# # VARIABLE NAMES
+# # cannot start w/ number
+# # case sensitive 'Test' is different to 'test'
+
+# # INPUT
+# # input("please write something")
+# # gives option user input in terminal
+# user_input = input("please write something ")
+# print(user_input)
+
+# EXERCISE
+user_input = input("Please enter your name ")
+print(f"Hello {user_input} have a nice day!")
+# There are lots of ways to add different variables, but the easiest way seems to be by using f-string formatting
+# https://www.geeksforgeeks.org/g-fact-25-print-single-multiple-variable-python/
