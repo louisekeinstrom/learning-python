9068457856257866287154ae05c083c0fef0350c
Louise Keinström <louise.keinstrom@outlook.com>
2024-07-16 18:09:15 +0200

    added set & dictionary

--- containers.py
+++ containers.py
@@ -1,4 +1,7 @@
 # Tuple=("hej", 123, ("another", "tuple"))
 # List = ["hej", 123, ["another", "list"]]
 
-# A tuple cannot change (immutable) while you can change the values of a list
+# # A tuple cannot change (immutable) while you can change the values of a list
+
+# Set = {1,2,3, "test"} #every entry has to be unique or theyre removed
+# Dictionary = {"name": "Sven", 123:"test"} #key with value
